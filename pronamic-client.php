319de2c5635708ea628b3e7cbeb0777335c3d192
Remco Tolsma <info@remcotolsma.nl>
2012-09-26 20:27:38 +0200

    Update plugin header comment.

--- pronamic-client.php
+++ pronamic-client.php
@@ -2,11 +2,20 @@
 /*
 Plugin Name: Pronamic Client
 Plugin URI: http://pronamic.eu/wordpress/pronamic-client/
-Description: WordPress plugin for Pronamic Clients
+Description: WordPress plugin for Pronamic clients.
+
 Version: 0.2
 Requires at least: 3.0
+
 Author: Pronamic
 Author URI: http://pronamic.eu/
+
+Text Domain: pronamic_client
+Domain Path: /languages/
+
+License: GPL
+
+GitHub URI: https://github.com/pronamic/wp-pronamic-client
 */
 
 /**
