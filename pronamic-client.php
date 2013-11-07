bd82e0cff4f60f7e8f3b77cb86e30b7dcd55c97d
Remco Tolsma <info@remcotolsma.nl>
2013-11-07 17:05:40 +0100

    Added check for Pronamic theme updates.

--- pronamic-client.php
+++ pronamic-client.php
@@ -32,53 +32,108 @@ function pronamic_client_init() {
 
 add_action('init', 'pronamic_client_init');
 
-function pronamic_client_transient_update_plugins_filter( $transient ) {
-	if ( empty( $transient->checked ) )
-		return $transient;
-
-	$pronamic_plugins = pronamic_client_get_plugins();
-//var_dump($pronamic_plugins);
-//exit;
-	foreach ( $pronamic_plugins as $file => $plugin ) {
-		$slug = pathinfo( $file, PATHINFO_DIRNAME );
-		$slug = $plugin['Name'];
-
-		if ( $slug !== 'pronamic-updater-test' )
-			continue;
-
-		$api_url = 'http://themes.pronamic.nl/2.0/';
-		$api_url = add_query_arg( 'slug', $slug, $api_url );
-
-		$response = wp_remote_get( $api_url );
-
-		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' ) {
-			break;
-		}
-		
-		$data = json_decode( wp_remote_retrieve_body( $response ) );
-
-		if ( $data ) {
-			$local_version  = $plugin['Version'];
-			$remote_version = $data->version;
-			
-			if ( version_compare( $remote_version, $local_version, '>' ) ) {
-				$plugin = array(
-					'slug'        => $slug,
-					'new_version' => $remote_version,
-					'url'         => null,
-					'package'     => $data->download_link,
-				);
-
-				$transient->response[$file] = (object) $plugin;
-			}
-		}
+/**
+ * @see https://github.com/WordPress/WordPress/blob/3.7/wp-includes/class-http.php#L268
+ */
+function pronamic_client_http_api_debug( $response, $name, $class, $args, $url ) {
+	if ( $url == 'https://api.wordpress.org/themes/update-check/1.1/' ) {
+		var_dump( $args );
+		exit;
 	}
-	
-	return $transient;
 }
 
-//add_filter( 'pre_set_site_transient_update_themes', 'pronamic_client_transient_update_themes_filter' );
+// add_action( 'http_api_debug', 'pronamic_client_http_api_debug', 10, 5 );
+
+
+/**
+ * @see https://github.com/WordPress/WordPress/blob/3.7/wp-includes/option.php#L1030
+ * 
+ * @param unknown $transient
+ * @return unknown
+ */
+function pronamic_client_transient_update_plugins_filter( $update_plugins ) {
+	$pronamic_plugins = pronamic_client_get_plugins();
+	
+	$options = array(
+		'timeout'    => ( ( defined('DOING_CRON') && DOING_CRON ) ? 30 : 3),
+		'body'       => array(
+			'plugins' => json_encode( $pronamic_plugins )
+		),
+		'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' )
+	);
+
+	$url = 'http://wp.pronamic.eu/api/plugins/update-check/1.0/';
+
+	$raw_response = wp_remote_post( $url, $options );
+
+	if ( is_wp_error( $raw_response ) || 200 != wp_remote_retrieve_response_code( $raw_response ) )
+		return $update_plugins;
+
+	$response = json_decode( wp_remote_retrieve_body( $raw_response ), true );
+	foreach ( $response['plugins'] as &$plugin ) {
+		$plugin = (object) $plugin;
+	}
+	unset( $plugin );
+
+	if ( is_array( $response ) ) {
+		$update_plugins->response = array_merge( $update_plugins->response, $response['plugins'] );
+	}
+	
+	return $update_plugins;
+}
+
 add_filter( 'pre_set_site_transient_update_plugins', 'pronamic_client_transient_update_plugins_filter' );
+
+/**
+ * @see https://github.com/WordPress/WordPress/blob/3.7/wp-includes/option.php#L1030
+ * 
+ * @param unknown $transient
+ * @return unknown
+ */
+function pronamic_client_transient_update_themes_filter( $update_themes ) {
+	$pronamic_themes = pronamic_client_get_themes();
+
+	$themes = array();
+
+	foreach ( $pronamic_themes as $theme ) {
+		$checked[ $theme->get_stylesheet() ] = $theme->get('Version');
+	
+		$themes[ $theme->get_stylesheet() ] = array(
+			'Name'       => $theme->get('Name'),
+			'Title'      => $theme->get('Name'),
+			'Version'    => $theme->get('Version'),
+			'Author'     => $theme->get('Author'),
+			'Author URI' => $theme->get('AuthorURI'),
+			'Template'   => $theme->get_template(),
+			'Stylesheet' => $theme->get_stylesheet(),
+		);
+	}
+
+	$options = array(
+		'timeout'    => ( ( defined('DOING_CRON') && DOING_CRON ) ? 30 : 3),
+		'body'       => array(
+			'themes' => json_encode( $themes )
+		),
+		'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' )
+	);
+
+	$url = 'http://wp.pronamic.eu/api/themes/update-check/1.0/';
+
+	$raw_response = wp_remote_post( $url, $options );
+
+	if ( is_wp_error( $raw_response ) || 200 != wp_remote_retrieve_response_code( $raw_response ) )
+		return $update_themes;
+
+	$response = json_decode( wp_remote_retrieve_body( $raw_response ), true );
+
+	if ( is_array( $response ) ) {
+		$update_themes->response = array_merge( $update_themes->response, $response['themes'] );
+	}
+
+	return $update_themes;
+}
+
+add_filter( 'pre_set_site_transient_update_themes', 'pronamic_client_transient_update_themes_filter' );
 
 function pronamic_client_get_plugins() {
 	// @see https://github.com/afragen/github-updater/blob/1.7.4/classes/class-plugin-updater.php#L68
@@ -87,7 +142,7 @@ function pronamic_client_get_plugins() {
 	$pronamic_plugins = array();
 	
 	foreach ( $plugins as $file => $plugin ) {
-		if ( isset( $plugin['Author'] ) && $plugin['Author'] == 'Pronamic' ) {
+		if ( isset( $plugin['Author'] ) && strpos( $plugin['Author'], 'Pronamic' ) !== false ) {
 			$pronamic_plugins[$file] = $plugin;
 		}
 	}
@@ -101,11 +156,11 @@ function pronamic_client_get_themes() {
 	$pronamic_themes = array();
 	
 	foreach ( $themes as $slug => $theme ) {
-		if ( isset( $theme['Author'] ) && $theme['Author'] == 'Pronamic' ) {
+		if ( isset( $theme['Author'] ) && strpos( $theme['Author'], 'Pronamic' ) !== false ) {
 			$pronamic_themes[$slug] = $theme;
 		}
 	}
-	
+
 	return $pronamic_themes;
 }
 
