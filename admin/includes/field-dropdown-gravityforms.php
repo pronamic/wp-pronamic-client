be0c3ec2a6d23275799ef958be6da76e997eb9a8
Remco Tolsma <info@remcotolsma.nl>
2014-04-25 16:07:43 +0200

    Fixed text and text domain.

--- admin/includes/field-dropdown-gravityforms.php
+++ admin/includes/field-dropdown-gravityforms.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'pronamic_field_dropdown_gravityforms' ) ) {
 		}
 
 		if ( empty( $forms ) ) {
-			_e( 'You don\'t  have any Gravity Forms forms.', 'pronamic_framework' );
+			_e( 'You don\'t have any Gravity Forms forms.', 'pronamic_client' );
 		} else {
 			$form_id = get_option( $name );
 
@@ -26,7 +26,7 @@ if ( ! function_exists( 'pronamic_field_dropdown_gravityforms' ) ) {
 				'<option value="%s" %s>%s</option>',
 				'',
 				selected( $form_id, '', false ),
-				__( '&mdash; Select a form &mdash;', 'pronamic_framework' )
+				__( '&mdash; Select a form &mdash;', 'pronamic_client' )
 			);
 
 			foreach ( $forms as $form ) {
