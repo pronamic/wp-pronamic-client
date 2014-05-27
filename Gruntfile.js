6c8540cad567d3c4f825e18d15fbfa1fe66ed797
Remco Tolsma <info@remcotolsma.nl>
2014-05-27 12:29:03 +0200

    Improved Grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function( grunt ) {
 	grunt.initConfig( {
 		// Package
 		pkg: grunt.file.readJSON( 'package.json' ),
-		
+
 		// PHPLint
 		phplint: {
 			options: {
@@ -13,15 +13,17 @@ module.exports = function( grunt ) {
 			},
 			all: [ '**/*.php' ]
 		},
-		
+
 		// PHP Code Sniffer
 		phpcs: {
-		    application: {
-		        dir: [ '**/*.php' ]
-		    },
-		    options: {
-		        standard: 'project.ruleset.xml'
-		    }
+			application: {
+				dir: [ './' ],
+			},
+			options: {
+				standard: 'project.ruleset.xml',
+				extensions: 'php',
+				ignore: 'wp-svn,deploy,node_modules'
+			}
 		},
 
 		// Check WordPress version
@@ -48,10 +50,51 @@ module.exports = function( grunt ) {
 				options: {
 					cwd: '',
 					domainPath: 'languages',
-					type: 'wp-plugin'
+					type: 'wp-plugin',
+					exclude: [ 'deploy/.*', 'wp-svn/.*' ],
 				}
 			}
-		}
+		},
+
+		// Copy
+		copy: {
+			deploy: {
+				src: [
+					'**',
+					'!.*',
+					'!.*/**',
+					'!composer.json',
+					'!Gruntfile.js',
+					'!package.json',
+					'!project.ruleset.xml',
+					'!node_modules/**',
+					'!wp-svn/**'
+				],
+				dest: 'deploy',
+				expand: true,
+				dot: true
+			},
+		},
+
+		// Clean
+		clean: {
+			deploy: {
+				src: [ 'deploy' ]
+			},
+		},
+
+		// WordPress deploy
+		rt_wp_deploy: {
+			app: {
+				options: {
+					svnUrl: 'http://plugins.svn.wordpress.org/pronamic-client/',
+					svnDir: 'wp-svn',
+					svnUsername: 'pronamic',
+					deployDir: 'deploy',
+					version: '<%= pkg.version %>',
+				}
+			}
+		},
 	} );
 
 	// Load tasks
@@ -59,8 +102,22 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-phpcs' );
 	grunt.loadNpmTasks( 'grunt-checkwpversion' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-rt-wp-deploy' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [ 'phplint', 'phpcs', 'checkwpversion' ] );
 	grunt.registerTask( 'pot', [ 'makepot' ] );
+
+	grunt.registerTask( 'deploy', [
+		'checkwpversion',
+		'clean:deploy',
+		'copy:deploy'
+	] );
+
+	grunt.registerTask( 'wp-deploy', [
+		'deploy',
+		'rt_wp_deploy'
+	] );
 };
