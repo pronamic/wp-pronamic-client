0aeae138de35812a19ab53ef6c6039074d3b211f
Remco Tolsma <info@remcotolsma.nl>
2014-12-15 13:24:25 +0100

    Added Grunt deploy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,38 @@ module.exports = function( grunt ) {
 					exclude: [ 'deploy/.*', 'wp-svn/.*' ],
 				}
 			}
+		},	
+		// Check textdomain errors
+		checktextdomain: {
+			options:{
+				text_domain: 'pronamic_client',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php',
+					'!deploy/**',
+					'!node_modules/**',
+					'!tests/**',
+					'!wp-content/**'
+				],
+				expand: true
+			}
 		},
 
 		// Copy
@@ -61,63 +93,132 @@ module.exports = function( grunt ) {
 			deploy: {
 				src: [
 					'**',
-					'!.*',
-					'!.*/**',
 					'!composer.json',
+					'!composer.lock',
 					'!Gruntfile.js',
 					'!package.json',
-					'!project.ruleset.xml',
+					'!phpunit.xml',
+					'!phpunit.xml.dist',
+					'!phpcs.ruleset.xml',
+					'!CHANGELOG.md',
+					'!README.md',
+					'!deploy/**',
+					'!documentation/**',
 					'!node_modules/**',
-					'!wp-svn/**'
+					'!tests/**',
+					'!wp-content/**',
 				],
-				dest: 'deploy',
-				expand: true,
-				dot: true
+				dest: 'deploy/latest',
+				expand: true
 			},
 		},
 
 		// Clean
 		clean: {
 			deploy: {
-				src: [ 'deploy' ]
+				src: [ 'deploy/latest' ]
 			},
 		},
 
+		// Compress
+		compress: {
+			deploy: {
+				options: {
+					archive: 'deploy/archives/<%= pkg.name %>.<%= pkg.version %>.zip'
+				},
+				expand: true,
+				cwd: 'deploy/latest',
+				src: ['**/*'],
+				dest: '<%= pkg.name %>/'
+			}
+		},
+
+		// Git checkout
+		gitcheckout: {
+			tag: {
+				options: {
+					branch: 'tags/<%= pkg.version %>'
+				}
+			},
+			develop: {
+				options: {
+					branch: 'develop'
+				}
+			}
+		},
+
+		// S3
+		aws_s3: {
+			options: {
+				region: 'eu-central-1'
+			},
+			deploy: {
+				options: {
+					bucket: 'downloads.pronamic.eu',
+					differential: true
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'deploy/archives/',
+						src: '<%= pkg.name %>.<%= pkg.version %>.zip',
+						dest: 'plugins/<%= pkg.name %>/'
+					}
+				]
+			}
+		},
+		
 		// WordPress deploy
 		rt_wp_deploy: {
 			app: {
 				options: {
-					svnUrl: 'http://plugins.svn.wordpress.org/pronamic-client/',
-					svnDir: 'wp-svn',
+					svnUrl: 'http://plugins.svn.wordpress.org/<%= pkg.name %>/',
+					svnDir: 'deploy/wp-svn',
 					svnUsername: 'pronamic',
-					deployDir: 'deploy',
+					deployDir: 'deploy/latest',
 					version: '<%= pkg.version %>',
 				}
 			}
-		},
+		}
 	} );
 
 	// Load tasks
 	grunt.loadNpmTasks( 'grunt-phplint' );
 	grunt.loadNpmTasks( 'grunt-phpcs' );
-	grunt.loadNpmTasks( 'grunt-checkwpversion' );
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-composer' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-checkwpversion' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-git' );
+	grunt.loadNpmTasks( 'grunt-aws-s3' );
 	grunt.loadNpmTasks( 'grunt-rt-wp-deploy' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [ 'phplint', 'phpcs', 'checkwpversion' ] );
-	grunt.registerTask( 'pot', [ 'makepot' ] );
+	grunt.registerTask( 'pot', [ 'checktextdomain', 'makepot' ] );
 
 	grunt.registerTask( 'deploy', [
-		'checkwpversion',
+		'default',
 		'clean:deploy',
-		'copy:deploy'
+		'copy:deploy',
+		'compress:deploy'
 	] );
 
 	grunt.registerTask( 'wp-deploy', [
+		'gitcheckout:tag',
 		'deploy',
-		'rt_wp_deploy'
+		'rt_wp_deploy',
+		'gitcheckout:develop'
+	] );
+	
+	grunt.registerTask( 's3-deploy', [
+		'gitcheckout:tag',
+		'deploy',
+		'aws_s3:deploy',
+		'gitcheckout:develop'
 	] );
 };
