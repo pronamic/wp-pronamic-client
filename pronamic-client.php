65e4f2d2d7caf99beb6ecd954f04211b441c400b
Karel-Jan Tolsma <kareljan@pronamic.nl>
2020-01-06 15:05:23 +0100

    Tracking code integration.

--- pronamic-client.php
+++ pronamic-client.php
@@ -24,7 +24,7 @@ require_once $dir . 'includes/class-plugin.php';
 require_once $dir . 'includes/class-updater.php';
 require_once $dir . 'includes/class-admin.php';
 require_once $dir . 'includes/class-settings.php';
-require_once $dir . 'includes/class-scripts.php';
+require_once $dir . 'includes/class-tracking.php';
 
 /**
  * Bootstrap
