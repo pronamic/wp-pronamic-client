868710cdd5d1840d058fd32835df8cbc8b736634
Remco Tolsma <info@remcotolsma.nl>
2020-12-10 11:18:27 +0100

    Patch "Easy SwipeBox" plugin.

--- classes/ScriptsModule.php
+++ classes/ScriptsModule.php
@@ -7,7 +7,6 @@ class ScriptsModule {
 	 * Instance of this class.
 	 *
 	 * @since 1.8.1
-	 *
 	 * @var Tracking
 	 */
 	protected static $instance = null;
@@ -32,24 +31,48 @@ class ScriptsModule {
 	 * resulted in a black overlay.
 	 *
 	 * @link https://github.com/brutaldesign/swipebox/issues/383
+	 * @link https://make.wordpress.org/core/2020/11/05/updating-core-jquery-to-version-3-part-2/
+	 * @link https://jquery.com/upgrade-guide/1.9/#selector-property-on-jquery-objects
+	 * @link https://developer.wordpress.org/reference/hooks/wp_print_scripts/
+	 * @link https://developer.wordpress.org/reference/functions/wp_deregister_script/
+	 * @link https://developer.wordpress.org/reference/functions/wp_script_is/
 	 * @return void
 	 */
 	public function patch_swipebox() {
-		if ( ! \wp_script_is( 'swipebox' ) ) {
-			return;
-		}
+		$handles = array(
+			/**
+			 * Pronamic themes register with the handle name 'swipebox':
+			 * @link https://gitlab.com/pronamic-themes/timmy/-/blob/ed073aa2876e2b0ba9582201d0378a253193edb1/classes/class-scripts.php#L65-72
+			 */
+			'swipebox',
+			/**
+			 * WordPress plugin 'Easy SwipeBox' register with the handle name 'easy-swipebox':
+			 * @link https://github.com/leopuleo/easy-swipebox/blob/1.1.1/public/class-easy-swipebox-public.php#L135-L148
+			 * @link https://github.com/leopuleo/easy-swipebox/blob/1.1.1/includes/class-easy-swipebox.php#L109
+			 */
+			'easy-swipebox',
+		);
+
+		/**
+		 * Filter the enqueued scripts.
+		 */
+		$handles = \array_filter( $handles, function( $handle ) {
+			return \wp_script_is( $handle );
+		} );
 
 		$min = \SCRIPT_DEBUG ? '' : '.min';
 
-		\wp_deregister_script( 'swipebox' );
+		foreach ( $handles as $handle ) {
+			\wp_deregister_script( $handle );
 
-		\wp_register_script(
-			'swipebox',
-			\plugins_url( '../assets/swipebox/js/jquery.swipebox' . $min . '.js', __FILE__ ),
-			array( 'jquery' ),
-			'mho79-1.4.4',
-			true
-		);
+			\wp_register_script(
+				$handle,
+				\plugins_url( '../assets/swipebox/js/jquery.swipebox' . $min . '.js', __FILE__ ),
+				array( 'jquery' ),
+				'mho79-1.4.4',
+				true
+			);
+		}
 	}
 
 	/**
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: pronamic, remcotolsma
 Tags: pronamic, client, update, plugin, theme, extension, plugins, themes, extensions
 Requires at least: 3.0
 Tested up to: 5.5.1
-Stable tag: 1.8.1
+Stable tag: 1.8.2
 
 WordPress plugin for Pronamic clients.
 
@@ -27,6 +27,9 @@ WordPress installation and then activate the Plugin from Plugins page.
 
 
 == Changelog ==
+
+= 1.8.2 =
+*	Patch "Easy SwipeBox" plugin.
 
 = 1.8.1 =
 *	Patch Swipebox.
