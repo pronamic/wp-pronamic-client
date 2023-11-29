c2b629b79adfd1e073a46cbb42f3be8688d1f660
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-11-29 12:00:13 +0100

    Modernize plugin headers for WP-CLI `dist-archive` command.

--- pronamic-client.php
+++ pronamic-client.php
@@ -1,22 +1,24 @@
 <?php
-/*
-Plugin Name: Pronamic Client
-Plugin URI: https://www.pronamic.eu/plugins/pronamic-client/
-Description: WordPress plugin for Pronamic clients.
-
-Version: 2.0.0
-Requires at least: 3.0
-
-Author: Pronamic
-Author URI: http://www.pronamic.eu/
-
-Text Domain: pronamic-client
-Domain Path: /languages/
-
-License: GPL
-
-GitHub URI: https://github.com/pronamic/wp-pronamic-client
-*/
+/**
+ * Pronamic Client
+ *
+ * @package   PronamicClient
+ * @author    Pronamic
+ * @copyright 2023 Pronamic
+ * 
+ * @wordpress-plugin
+ * Plugin Name: Pronamic Client
+ * Plugin URI: https://wp.pronamic.directory/plugins/pronamic-client/
+ * Description: WordPress plugin for Pronamic clients.
+ * Version: 2.0.0
+ * Requires at least: 3.0
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: pronamic-client
+ * Domain Path: /languages/
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
